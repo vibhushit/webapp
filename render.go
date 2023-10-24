e62f2965876ec1c3b6a7fe47d5657974b32d1130
vibhushit <vibhushit10@gmail.com>
2023-10-24 15:34:34 +0530

    reorganised the files

---
+++ render.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("Error parsing template", err)
+		return
+	}
+}
